364c9b4050a489acb7e292333c2f4c428213e1ad
Muhammad Affan Rasheed <affanrasheed2751@gmail.com>
2024-11-16 18:30:37 +0500

    Update problem3.c

--- Assignment 2/Question 3/problem3.c
+++ Assignment 2/Question 3/problem3.c
@@ -1,36 +1,32 @@
 #include <stdio.h>
-int main(){
-    int n,i,j,removed;
-    printf("enter number of words you want to enter: ");
-    scanf("%d",&n);
+#include <string.h>
+
+int main() {
+    int n, i, j;
+    int removed;
+    printf("Enter number of words you want to enter: ");
+    scanf("%d", &n);
     char words[n][30];
-    char tmp[30];
-    for (i = 0; i < n; i++)
-    {
-        printf("enter word number %d: ",i+1);\
-        scanf(&words[i]);
-    }
-    
-    for (j = 0; j < n; j++)
-    {
+
+    for (i = 0; i < n; i++) {
+        printf("Enter word number %d: ", i + 1);
+        scanf("%s", words[i]);
+        int len = strlen(words[i]);
+        int index = 0;
         removed = 0;
-        for (j = 1; j < 30; j++)
-        {
-            if (words[i][j] = '\0' )
-            {
-                break;
+        for (j = 0; j < strlen(words[i]); j++) {
+            if (j == 0 || words[i][j] != words[i][j - 1]) {
+                words[i][index] = words[i][j];
+                index+=1;
             }
-            else if(words[i][j] != words[i][j+1]){
-                strcat(tmp,words[i][j]);
-            }
-            else
-            {
+            else{
                 removed++;
             }
         }
-        strcpy(words[i],tmp);
-        printf("the  compressed word number %d is %s and the removed characters are %d\n",i+1,words[i],removed);   
+        words[i][index] = '\0';
+
+        printf("The compressed word number %d is %s and the number of characters removed are %d\n", i + 1, words[i],removed);
     }
-    
-   
+
+    return 0;
 }
